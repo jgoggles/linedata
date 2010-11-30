d1df40a73845c8894f2ff67d64ccbaa2fbb650c4
Dan <daniel.weaver@gmail.com>
2010-11-29 20:01:18 -0700

    basic home v away and fav v dog ats

--- game.rb
+++ game.rb
@@ -1,33 +1,48 @@
 require 'config.rb'
 
 class Game < ActiveRecord::Base
-  def self.ats(team)
+  def self.ats
     wins = 0
     losses = 0
     pushes = 0
     all.each do |g|
-      if team == "home"
-        if (g.home_score.to_f + g.spread) > g.away_score.to_f
-          wins += 1
-        elsif (g.home.to_f + g.spread) < g.away_score.to_f
-          losses += 1
-        elsif (g.home.to_f + g.spread) == g.away_score.to_f
-          pushes += 1
-          puts g.id
-        end
-      else
+      if (g.home_score.to_f - g.spread) > g.away_score.to_f
+        wins += 1
+      elsif (g.home_score.to_f - g.spread) < g.away_score.to_f
+        losses += 1
+      elsif (g.home_score.to_f - g.spread) == g.away_score.to_f
+        pushes += 1
+      end
+    end  
+    puts "Home - (W):#{wins} (L):#{losses} (P):#{pushes}"
+  end
+
+  def self.ats_2
+    wins = 0
+    losses = 0
+    pushes = 0
+    all.each do |g|
+      if g.spread > 0
         if (g.away_score.to_f + g.spread) > g.home_score.to_f
           wins += 1
         elsif (g.away_score.to_f + g.spread) < g.home_score.to_f
           losses += 1
         elsif (g.away_score.to_f + g.spread) == g.home_score.to_f
           pushes += 1
-          puts g.id
+        end
+      elsif g.spread < 0
+        if (g.home_score.to_f - g.spread) > g.away_score.to_f
+          wins += 1
+        elsif (g.home_score.to_f - g.spread) < g.away_score.to_f
+          losses += 1
+        elsif (g.home_score.to_f - g.spread) == g.away_score.to_f
+          pushes += 1
         end
       end
-    end  
-    puts "#{wins}-#{losses}-#{pushes}"
+    end
+    puts "Favorites - (W):#{wins} (L):#{losses} (P):#{pushes}"
   end
 end
 
-Game.ats("home")
+Game.ats
+Game.ats_2
